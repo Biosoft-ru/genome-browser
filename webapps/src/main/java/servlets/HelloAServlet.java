5dbdc522bb501f8b691fb955c88b62d6f6dbd128
anna <anna@developmentontheedge.com>
2025-02-04 14:23:06 +0700

    copy web related classes to wepapps subpackage. servlet test works.

---
+++ webapps/src/main/java/servlets/HelloAServlet.java
@@ -0,0 +1,51 @@
+//
+// ========================================================================
+// Copyright (c) 1995 Mort Bay Consulting Pty Ltd and others.
+//
+// This program and the accompanying materials are made available under the
+// terms of the Eclipse Public License v. 2.0 which is available at
+// https://www.eclipse.org/legal/epl-2.0, or the Apache License, Version 2.0
+// which is available at https://www.apache.org/licenses/LICENSE-2.0.
+//
+// SPDX-License-Identifier: EPL-2.0 OR Apache-2.0
+// ========================================================================
+//
+
+package servlets;
+
+import java.io.IOException;
+import jakarta.servlet.ServletConfig;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+public class HelloAServlet extends HttpServlet
+{
+    private String msg;
+
+    @Override
+    public void init(ServletConfig config) throws ServletException
+    {
+        super.init(config);
+
+        msg = config.getInitParameter("message");
+        if (msg == null)
+        {
+            msg = "Default HelloAServlet response";
+        }
+    }
+
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException
+    {
+        response.setContentType("text/plain");
+        response.getWriter().printf("Get query %s%n", msg);
+    }
+
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException
+    {
+        response.setContentType("text/plain");
+        response.getWriter().printf("POST query %s%n", msg);
+    }
+}
